b421ceaa7300fe41711ab4964351855e0f823be3
saikishore0319 <saikishor20172017@gmail.com>
2026-04-03 13:38:22 +0000

    backend pushed

---
+++ frontend/src/auth/config.js
@@ -0,0 +1,6 @@
+// src/auth/config.js
+export const COGNITO_CONFIG = {
+  domain: "https://ap-south-1fb6pjoavw.auth.ap-south-1.amazoncognito.com",
+  clientId: "7lpjf6s8nirnvnof49mpeds82h",
+  redirectUri: "https://d3o9zq93erzzx5.cloudfront.net"
+};
